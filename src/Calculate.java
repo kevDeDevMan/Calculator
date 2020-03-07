d69db38e3db47fe4710e96a38e9f1a2e7d941abf
Kevin <Kevin@Kevins-MacBook-Pro.local>
2020-03-07 13:02:09 -0500

    pushing my first loop try to remote

---
+++ src/Calculate.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Calculate {
+
+    public int calculate(){
+        Scanner readInput = new Scanner(System.in);
+        System.out.println("Enter what operation you want to do:");
+        String operator = readInput.next();
+        if (operator.equals("+")) {
+            System.out.println("Enter the first number you want to add:");
+            double addNum1 = readInput.nextDouble();
+            System.out.println("Enter the second number you want to add:");
+            double addNum2 = readInput.nextDouble();
+            double addResult = addNum1 + addNum2;
+            System.out.println("The result of your addition is: " + addResult);
+        }
+        if (operator.equals("-")) {
+            System.out.println("Enter the first number you want to subtract:");
+            double subNum1 = readInput.nextDouble();
+            System.out.println("Enter the second number you want to subtract:");
+            double subNum2 = readInput.nextDouble();
+            double subResult = subNum1 - subNum2;
+            System.out.println("The result of your subtraction is: " + subResult);
+        }
+        if (operator.equals("*")) {
+            System.out.println("Enter the first number you want to multiply:");
+            double timesNum1 = readInput.nextDouble();
+            System.out.println("Enter the second number you want to multiply:");
+            double timesNum2 = readInput.nextDouble();
+            double timesResult = timesNum1 * timesNum2;
+            System.out.println("The result of your multiplication is: " + timesResult);
+        }
+        if (operator.equals("/")) {
+            System.out.println("Enter the first number you want to divide:");
+            double divNum1 = readInput.nextDouble();
+            System.out.println("Enter the second number you want to divide:");
+            double divNum2 = readInput.nextDouble();
+            double divResultFlat = divNum1 / divNum2;
+            System.out.println("The result of your division is: " + divResultFlat);
+        }
+        return -1;
+    }
+
+    public boolean continueMath(){
+        Scanner continueMath = new Scanner(System.in);
+        boolean loopMath = continueMath.nextBoolean();
+        do {
+            calculate();
+        } while (loopMath == true);
+        return false;
+    }
+
+}
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,42 +1,10 @@
-import java.util.Scanner;
-
-public class Calculator {
+public class Calculator extends Calculate {
 
     public static void main(String[] args) {
         System.out.println("Welcome to my calculator.");
-        Scanner readInput = new Scanner(System.in);
+        Calculator equation = new Calculator();
 
-        System.out.println("Enter what operation you want to do:");
-        String operator = readInput.next();
-
-        if (operator.equals("+")) {
-            System.out.println("Enter the first number you want to add:");
-            double addNum1 = readInput.nextDouble();
-            System.out.println("Enter the second number you want to add:");
-            double addNum2 = readInput.nextDouble();
-            double addResult = addNum1 + addNum2;
-            System.out.println("The result of your addition is: " + addResult);}
-        if (operator.equals("-")) {
-            System.out.println("Enter the first number you want to subtract:");
-            double subNum1 = readInput.nextDouble();
-            System.out.println("Enter the second number you want to subtract:");
-            double subNum2 = readInput.nextDouble();
-            double subResult = subNum1 - subNum2;
-            System.out.println("The result of your subtraction is: " + subResult);}
-        if (operator.equals("*")) {
-            System.out.println("Enter the first number you want to multiply:");
-            double timesNum1 = readInput.nextDouble();
-            System.out.println("Enter the second number you want to multiply:");
-            double timesNum2 = readInput.nextDouble();
-            double timesResult = timesNum1 * timesNum2;
-            System.out.println("The result of your multiplication is: " + timesResult);}
-        if (operator.equals("/")) {
-            System.out.println("Enter the first number you want to divide:");
-            double divNum1 = readInput.nextDouble();
-            System.out.println("Enter the second number you want to divide:");
-            double divNum2 = readInput.nextDouble();
-            double divResultFlat = divNum1 / divNum2;
-            System.out.println("The result of your division is: " + divResultFlat);
-                    }
-                }
+        equation.calculate();
+        equation.continueMath();
+    }
             }
